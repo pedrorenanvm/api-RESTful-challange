589633305e27035b439cf28dc15fd6a0dc66ac51
Pedro Renan <pedroviana15094@gmail.com>
2024-07-08 15:09:53 -0300

    BugFix CORS

--- src/main/java/br/com/pedrorenan/api_REST_ful_java/Application.java
+++ src/main/java/br/com/pedrorenan/api_REST_ful_java/Application.java
@@ -1,8 +1,10 @@
 package br.com.pedrorenan.api_REST_ful_java;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = @Server(url = "/", description = "Default Server URL"))
 @SpringBootApplication
 public class Application {
 
